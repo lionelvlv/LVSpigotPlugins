39ca0248940a4b2e82e85c4fc8360c0b0121f7c5
Lionel Verano <lionel.verano.iv@berkeley.edu>
2023-08-01 21:18:43 -0700

    Redefined permission nodes

--- CommandsPractice2/src/main/java/me/lionelv/commandspractice2/commands/GodCommand.java
+++ CommandsPractice2/src/main/java/me/lionelv/commandspractice2/commands/GodCommand.java
@@ -9,17 +9,21 @@ import org.bukkit.entity.Player;
 public class GodCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player){
+        if (commandSender instanceof Player) {
             Player p = (Player) commandSender;
-            if (p.isInvulnerable()){
-                p.setInvulnerable(false);
-                p.sendMessage(ChatColor.RED + "Godmode disabled.");
-            }
-            else{
-                p.setInvulnerable(true);
-                p.sendMessage(ChatColor.GREEN + "Godmode enabled");
-            }
+            if (p.hasPermission("lv.command.god")) {
+                if (p.isInvulnerable()) {
+                    p.setInvulnerable(false);
+                    p.sendMessage(ChatColor.RED + "Godmode disabled.");
+                } else {
+                    p.setInvulnerable(true);
+                    p.sendMessage(ChatColor.GREEN + "Godmode enabled");
+                }
 
+            }
+            else {
+                p.sendMessage("You do not have permission to run this command!");
+            }
         }
 
         //prevents the command being printed back to us
--- CommandsPractice2/src/main/java/me/lionelv/commandspractice2/commands/feed.java
+++ CommandsPractice2/src/main/java/me/lionelv/commandspractice2/commands/feed.java
@@ -9,13 +9,18 @@ import org.bukkit.entity.Player;
 public class feed implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player p){
-            if(p.getFoodLevel() < 20){
-                p.setFoodLevel(20);
-                p.sendMessage(ChatColor.YELLOW + "Bon appetit!");
+        if (commandSender instanceof Player p) {
+            if (p.hasPermission("lv.command.feed")) {
+                if (p.getFoodLevel() < 20) {
+                    p.setFoodLevel(20);
+                    p.sendMessage(ChatColor.YELLOW + "Bon appetit!");
+                } else {
+                    p.sendMessage(ChatColor.RED + "You're full!");
+
+                }
             }
             else {
-                p.sendMessage(ChatColor.RED + "You're full!");
+                p.sendMessage("You do not have permission to run this command!");
             }
         }
         else {
